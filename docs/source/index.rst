05f2ef4efb1dd6f2a9c04698665fc7a0cd3aa429
Alan Griffiths <algriff@gmail.com>
2015-12-16 15:22:25 +1100

    Start with template from https://github.com/seanfisk/python-project-template

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Radon Detector Deconvolution
+============================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ rddeconv/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'rddeconv'
+project = "Radon Detector Deconvolution"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Correct (deconvolve) the output from two-filter radon detectors'
+authors = ['Alan Griffiths']
+authors_string = ', '.join(authors)
+emails = ['algriff@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'http://example.com/'
