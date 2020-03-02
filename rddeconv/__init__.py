f150157b3436a760e671a5f6a51f412484d260a9
Alan Griffiths <algriff@gmail.com>
2020-03-02 14:00:33 +1100

    Begun working on simpler api

--- rddeconv/__init__.py
+++ rddeconv/__init__.py
@@ -7,3 +7,9 @@ __version__ = metadata.version
 __author__ = metadata.authors[0]
 __license__ = metadata.license
 __copyright__ = metadata.copyright
+
+
+from .deconvolve import deconvolve_timeseries
+
+# this is a helpful constant
+from theoretical_model import lamrn
