a2440f027e07536618f1338b04d9698f6f96a220
Alan Griffiths <algriff@gmail.com>
2022-05-09 15:08:19 +1000

    Remove unneeded files

--- rddeconv/fast_model/__init__.py
+++
@@ -1,5 +0,0 @@
-#from __future__ import absolute_import
-
-from .fast_detector import N_param, N_state, detector_model, parameter_array_from_dict, calc_steady_state, inverse_transform_radon_concs, inverse_transform_radon_concs_inplace, transform_radon_concs, transform_radon_concs_inplace
-
-
